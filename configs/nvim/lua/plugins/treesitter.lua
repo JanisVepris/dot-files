b573a3fcb37cf2e15ed0ec3c76283cd379b3376a
Janis Vepris <janisvepris@Mac.localdomain>
2025-12-02 01:48:04 +0200

    Update

--- configs/nvim/lua/plugins/treesitter.lua
+++ configs/nvim/lua/plugins/treesitter.lua
@@ -1,55 +1,80 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		config = function()
-			require("nvim-treesitter.configs").setup({
-				ensure_installed = {
-					"bash",
-					"css",
-					"diff",
-					"dockerfile",
-					"git_config",
-					"git_rebase",
-					"gitattributes",
-					"gitcommit",
-					"gitignore",
-					"graphql",
-					"go",
-					"html",
-					"ini",
-					"javascript",
-					"json",
-					"json5",
-					"lua",
-					"luadoc",
-					"markdown",
-					"markdown_inline",
-					"php",
-					"phpdoc",
-					"python",
-					"scss",
-					"sql",
-					"typescript",
-					"vim",
-					"vimdoc",
-					"vue",
-					"yaml",
-				},
+    {
+        "nvim-treesitter/nvim-treesitter",
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter-textobjects",
+        },
+        build = ":TSUpdate",
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                ensure_installed = {
+                    "bash",
+                    "css",
+                    "diff",
+                    "dockerfile",
+                    "git_config",
+                    "git_rebase",
+                    "gitattributes",
+                    "gitcommit",
+                    "gitignore",
+                    "graphql",
+                    "go",
+                    "html",
+                    "ini",
+                    "javascript",
+                    "json",
+                    "json5",
+                    "lua",
+                    "luadoc",
+                    "markdown",
+                    "markdown_inline",
+                    "php",
+                    "phpdoc",
+                    "python",
+                    "scss",
+                    "sql",
+                    "typescript",
+                    "vim",
+                    "vimdoc",
+                    "vue",
+                    "yaml",
+                },
 
-				sync_install = false,
+                sync_install = false,
 
-				auto_install = true,
+                auto_install = true,
 
-				indent = {
-					enable = true,
-				},
+                indent = {
+                    enable = true,
+                },
 
-				highlight = {
-					enable = true,
-					additional_vim_regex_highlighting = { "markdown" },
-				},
-			})
-		end,
-	},
+                highlight = {
+                    enable = true,
+                    additional_vim_regex_highlighting = { "markdown" },
+                },
+
+                textobjects = {
+                    select = {
+                        enable = true,
+                        lookahead = true,
+                        keymaps = {
+                            ["af"] = "@function.outer",
+                            ["if"] = "@function.inner",
+                            ["as"] = { query = "@local.scope", query_group = "locals", desc = "Select language scope" },
+                            ["is"] = {
+                                query = "@local.scope.inner",
+                                query_group = "locals",
+                                desc = "Select language scope",
+                            },
+                        },
+                        selection_modes = {
+                            ["@parameter.outer"] = "v", -- charwise
+                            ["@function.outer"] = "V", -- linewise
+                            ["@class.outer"] = "<c-v>", -- blockwise
+                        },
+                    },
+                },
+            })
+        end,
+    },
 }
